3fd3e7c450970a1c768cec99954299b51764996e
wonyeehow <won@won.lan>
2016-02-23 10:41:31 +0800

    updated

--- assets/script.js
+++ assets/script.js
@@ -42,10 +42,10 @@ $(function(){
       });
 
       el.page.on('swipeup',function(e,data){
-        go('next');
+        go('prev');
       });
       el.page.on('swipedown',function(e,data){
-        go('prev');
+        go('next');
       });
 
       var go = function(direction) {
@@ -79,10 +79,28 @@ $(function(){
           top: 0
         });
         current = next;
+        switch(current+1) {
+          // case 1:
+          //   yeehow.fxParallax($('#page1 .bg'));
+          //   break;
+          case 2:
+            fn.page2();
+            break;
+          case 3:
+            fn.page3();
+            break;
+          case 4:
+            fn.page4();
+            break;
+          case 5:
+            fn.page5();
+            break;
+        }
       };
 
       var reset = function() {
         el.page.attr('class', 'page').removeAttr('style');
+        $('#page2 .clone').remove();
       };
 
       function debounce(func, wait, immediate) {
@@ -117,10 +135,94 @@ $(function(){
           left: l + "%"
         });
       }
-      window.addEventListener("mousemove", touches);
-      window.addEventListener("touchstart", touches);
-      window.addEventListener("touchmove", touches);
+      $('#page1').bind('mousemove touchstart touchmove', touches);
+      // $('#page1').addEventListener("mousemove", touches);
+      // $('#page1').addEventListener("touchstart", touches);
+      // $('#page1').addEventListener("touchmove", touches);
     })(),
+    page2: function() {
+      var depth = 50;
+      var clone = $($('#page2 .bg').html().replace('svg-container', 'svg-container clone'));
+      // var shadow = $('<div class="shadow"></div>');
+
+      // shadow.appendTo('#page2 .bg');
+
+      for (var i = 1; i <= depth; i++) {
+        var d = i * 0.05;
+
+        clone.clone().appendTo($('#page2 .bg'))
+          .css({
+            'margin-top': i,
+            'margin-left': depth - i,
+            'z-index': depth - i
+          })
+        TweenMax.from($('#page2 .clone').eq(i-1), 2, {autoAlpha: 0, delay: d});
+      }
+    },
+    page3: function() {
+      fn.anibg($('#page3 .drawsvg'));
+    },
+    page4: function() {
+      fn.anibg($('#page4 .drawsvg'));
+    },
+    anibg: function(el) {
+      var svg = el.drawsvg({
+        duration: 4000,
+        stagger: 2000,
+      });
+      svg.drawsvg('animate');
+    },
+    page5: function() {
+      var colorlist = ['#8b026f', '#9c2684', '#9e418b', '#a15391', '#a66699'];
+      function random(min, max){
+        return (Math.random() * (max - min)) + min;
+      }
+
+      $('#page5 .bg').append('<div id="page5_bubble"></div>');
+
+      for (var i=0; i<100; i++) {
+        var size = random(20, 40).toFixed();
+        var alpha = random(0.3, 0.6).toFixed(1);
+        var color = random(0, 4).toFixed();
+        var bubble = $('<div class="bubble"></div>')
+        var w = window.innerWidth;
+        var h = window.innerHeight;
+        var x = random(0, w),
+            y = random(0, h),
+            z = random(-1000, -200);
+
+        bubble.appendTo('#page5_bubble').css({
+          'height': size,
+          'width': size,
+          'background-color': colorlist[color]
+        })
+        TweenMax.fromTo(bubble, 3, {
+            opacity: 0,
+            x: x,
+            y: y,
+            z: z
+          }, {
+            opacity: .5,
+            x: x,
+            y: y,
+            z: 500,
+            repeat: -1,
+            delay: i * -.015
+        });
+      }
+      var touches = function(e) {
+        var x = e.touches ? e.touches[0].clientX : e.clientX, 
+            y = e.touches ? e.touches[0].clientY : e.clientY;
+        
+        TweenMax.to($('#page5_bubble'), 1, {
+          webkitPerspectiveOrigin: x + "px " + y +"px",
+          perspectiveOrigin: x + "px " + y +"px"
+        });
+        
+      }
+
+      $('#page5_bubble').bind('mousemove touchstart touchmove', touches);
+    },
     desc: (function() {
       el.link.click(function(){
         var t = $(this);
